978414d1b6bbdd7fa4dda6051eebdfe3e6a724c8
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@grupoutp.net>
2024-10-04 16:37:46 -0500

    Primer Commit

---
+++ SemanaS8_V2/SemanaS8_V2/src/semanas8_v2/SemanaS8_V2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semanas8_v2;
+
+/**
+ *
+ * @author LAB-USR-CHIMBOTE
+ */
+public class SemanaS8_V2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
